bd1d65bdc40a3b008fb8f84100cd17d3a60604f3
mchels <mortenhels@gmail.com>
2016-11-10 13:02:52 +0100

    Use absolute paths in templates

--- templates/template.py
+++ templates/template.py
@@ -1,14 +1,16 @@
 import sys
+import os
 path_to_folderbrowser_directory = '..'
 sys.path.append(path_to_folderbrowser_directory)
 from folderbrowser import FolderBrowser
 from PyQt5 import QtWidgets
 import numpy as np
 
+cwd = os.getcwd()
 n_figs = 2
 win_t = 'Your title here'
-data_path = '../data'
-pcols_path = 'pcols.py'
+data_path = os.path.join(cwd, '../data')
+pcols_path = os.path.join(cwd, 'pcols.py')
 qApp = QtWidgets.QApplication(sys.argv)
 brw = FolderBrowser(n_figs, data_path, pcols_path)
 sys.exit(qApp.exec_())
